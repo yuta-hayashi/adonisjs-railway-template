375d887b75a6a844848c0a52324b5084fb9bce1a
yuta-hayashi <dev.hyuta@gmail.com>
2026-04-01 17:56:40 +0900

    use postgresql

--- start/env.ts
+++ start/env.ts
@@ -22,6 +22,13 @@ export default await Env.create(new URL('../', import.meta.url), {
   APP_KEY: Env.schema.secret(),
   APP_URL: Env.schema.string({ format: 'url', tld: false }),
 
+  // Database
+  DB_HOST: Env.schema.string({ format: 'host' }),
+  DB_PORT: Env.schema.number(),
+  DB_USER: Env.schema.string(),
+  DB_PASSWORD: Env.schema.string(),
+  DB_DATABASE: Env.schema.string(),
+
   // Session
   SESSION_DRIVER: Env.schema.enum(['cookie', 'memory', 'database'] as const),
 })
